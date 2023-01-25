64ff3e0836a04d3cc33d783cf868d20063a70696
Ati (chets) tesakulsiri <ksde34@gmail.com>
2023-01-25 17:58:16 +0700

    update

---
+++ HW1/model_training.py
@@ -0,0 +1,169 @@
+import time
+import torch
+import copy
+
+import torch.nn as nn
+
+
+def train_model(model, dataloaders, criterion, device, optimizer, num_epochs=5, weights_name='weight_save', is_inception=False,):
+    '''
+    train_model function
+    Train a PyTorch model for a given number of epochs.
+            Parameters:
+                    model: Pytorch model
+                    dataloaders: dataset
+                    criterion: loss function
+                    optimizer: update weights function
+                    num_epochs: number of epochs
+                    weights_name: file name to save weights
+                    is_inception: The model is inception net (Google LeNet) or not
+            Returns:
+                    model: Best model from evaluation result
+                    val_acc_history: evaluation accuracy history
+                    loss_acc_history: loss value history
+    '''
+    since = time.time()
+
+    val_acc_history = []
+    loss_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        epoch_start = time.time()
+
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over the train/validation dataset according to which phase we're in
+
+            for inputs, labels in dataloaders[phase]:
+
+                # Inputs is one batch of input images, and labels is a corresponding vector of integers
+                # labeling each image in the batch. First, we move these tensors to our target device.
+
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # Zero out any parameter gradients that have previously been calculated. Parameter
+                # gradients accumulate over as many backward() passes as we let them, so they need
+                # to be zeroed out after each optimizer step.
+
+                optimizer.zero_grad()
+
+                # Instruct PyTorch to track gradients only if this is the training phase, then run the
+                # forward propagation and optionally the backward propagation step for this iteration.
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    # The inception model is a special case during training because it has an auxiliary
+                    # output used to encourage discriminative representations in the deeper feature maps.
+                    # We need to calculate loss for both outputs. Otherwise, we have a single output to
+                    # calculate the loss on.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs, aux_outputs2 = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss3 = criterion(aux_outputs2, labels)
+                        loss = loss1 + 0.3 * (loss2*loss3)
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # Backpropagate only if in training phase
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # Gather our summary statistics
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double(
+            ) / len(dataloaders[phase].dataset)
+            epoch_end = time.time()
+
+            elapsed_epoch = epoch_end - epoch_start
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+            print("Epoch time taken: ", elapsed_epoch)
+
+            # If this is the best model on the validation set so far, deep copy it
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                torch.save(model.state_dict(), weights_name + ".pth")
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+            if phase == 'train':
+                loss_acc_history.append(epoch_loss)
+
+        print()
+
+    # Output summary statistics, load the best weight set, and return results
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history, loss_acc_history
+
+
+def evaluate(model, iterator, criterion, device):
+
+    total = 0
+    correct = 0
+    epoch_loss = 0
+    epoch_acc = 0
+
+    predicteds = []
+    trues = []
+
+    model.eval()
+
+    with torch.no_grad():
+
+        for batch, labels in iterator:
+
+            # Move tensors to the configured device
+            batch = batch.to(device)
+            labels = labels.to(device)
+
+            predictions = model(batch.float())
+
+            loss = criterion(predictions, labels.long())
+
+            predictions = nn.functional.softmax(predictions, dim=1)
+            # returns max value, indices
+            _, predicted = torch.max(predictions.data, 1)
+
+            predicteds.append(predicted)
+            trues.append(labels)
+            total += labels.size(0)  # keep track of total
+            # .item() give the raw number
+            correct += (predicted == labels).sum().item()
+            acc = 100 * (correct / total)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator), predicteds, trues
