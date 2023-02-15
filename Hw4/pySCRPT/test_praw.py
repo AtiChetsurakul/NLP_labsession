78388239c01399604389353325460e1d570fa4a8
Ati (chets) tesakulsiri <ksde34@gmail.com>
2023-02-15 15:31:22 +0700

    update

---
+++ Hw4/pySCRPT/test_praw.py
@@ -0,0 +1,19 @@
+import praw
+from secKey import ret_id
+
+
+if __name__ == '__main__':
+
+    client, secret, _ = ret_id()
+    reddit = praw.Reddit(client_id=client,
+
+                         client_secret=secret,
+
+                         user_agent='ati')
+
+    print(reddit.read_only)  # Output: True
+
+    subreddit = reddit.subreddit('apple')
+    topics = [*subreddit.top(limit=50)]  # top posts all time
+    print(len(topics))
+    fifty_sen = title = [n.title for n in topics]
